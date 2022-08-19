f09938ee509bd4b83b880be024201a67613d3aa4
Fernando Valle <fersoriano@ciencias.unam.mx>
2022-08-19 16:50:55 -0500

    add: factorial y suma resistencias en serie en carpeta math-physics

--- numbers, math, physics/1prograEjemplosConNumeros.py
+++ numbers, math, physics/1prograEjemplosConNumeros.py
@@ -1,4 +1,4 @@
-# 1.-Curzon Numbers
+### 1.-Curzon Numbers
 # In this challenge, establish if a given integer num is a Curzon number. If 1 plus
 # 2 elevated to num is exactly divisible by 1 plus 2 multiplied by num, then num
 # is a Curzon number. Given a non-negative integer num, implement a function that
@@ -14,7 +14,7 @@ def is_curzon(num):
 # print(is_curzon(10))
 # print(is_curzon(14))
 
-# 2.- Calculating Damage
+### 2.- Calculating Damage
 # Create a function that takes damage and speed (attacks per second) and 
 # returns the amount of damage after a given time.
 # FUENTE:https://edabit.com/challenge/HSHHkdRYXfgfZSqri
@@ -32,12 +32,58 @@ def damage(damage, speed, time):
     time_seconds = speed*60*60
     resulting_damage = damage * time_seconds
     return resulting_damage
+# print(damage(40, 5, "second"))
+# print(damage(100, 1, "minute"))
+# print(damage(2, 100, "hour"))
+# print(damage(-2, 100, "hour"))
 
-print(damage(40, 5, "second"))
-print(damage(100, 1, "minute"))
-print(damage(2, 100, "hour"))
-print(damage(-2, 100, "hour"))
+### 3.- Sum of Resistance in Series Circuits
+# When resistors are connected together in series, the same current passes through
+# each resistor in the chain and the total resistance, RT, of the circuit must be
+# equal to the sum of all the individual resistors added together. That is:
+# RT = R1 + R2 + R3 ...
+# Create a function that takes an array of values resistance that are connected 
+# in series, and calculates the total resistance of the circuit in ohms. The ohm
+# is the standard unit of electrical resistance in the International System of 
+# Units ( SI ).
+def series_resistance(lst):
+    suma = 0
+    for resistencia in lst:
+        suma = suma + resistencia
+    if suma>1:
+        return f'{suma} ohms'
+    return f'{suma} ohm'         
+# print(series_resistance([1, 5, 6, 3]))
 
+### 4.-Radians to Degrees
+# Create a function that takes an angle in radians and returns the corresponding 
+# angle in degrees rounded to one decimal place.
+# from math import pi
+def radians_to_degrees(rad):
+    from math import pi
+    degrees = (360 * rad) / (2*pi)
+    return round(degrees,1)
+# print(radians_to_degrees(50))
+
+### 5.-Return the Factorial
+# Create a function that takes an integer and returns the factorial of that integer.
+# That is, the integer multiplied by all positive lower integers.
+# FUENTE: https://edabit.com/challenge/FF6kYPHdAcJnoosr5
+def factorial(num):
+    if num > 0:
+        factorial = 1
+        lista_factorial = []
+        while num >= 1:
+            lista_factorial.append(num)
+            num = num - 1 
+        for num in lista_factorial:
+            factorial = factorial * num
+        return factorial
+# Versión con recursividad, vista en las soluciones del link :   
+def factorial(num):
+	return 1 if num < 2 else num * factorial(num - 1)
+        
+    
 
 
 
