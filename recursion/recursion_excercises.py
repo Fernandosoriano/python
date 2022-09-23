0864ffac6945357128c0fbf03e41267bccdb5dbb
Fernando Valle <fersoriano@ciencias.unam.mx>
2022-09-23 11:45:04 -0500

    add:XO, factorial(recursivo)

--- recursion/recursion_excercises.py
+++ recursion/recursion_excercises.py
@@ -9,4 +9,7 @@ def find_highest(lst):
         return max if max > lst[0] else lst[0]
 # find_highest = lambda lst:
 
-print(find_highest([-1, 3, 5, 6, 99, 12, 2]))
+# print(find_highest([-1, 3, 5, 6, 99, 12, 2]))
+factorial = lambda n: 1 if n == 1 else 1 if n == 0 else n * factorial(n-1)
+print(factorial(5))
+
