b15b125646b5b0dfe640c92c53d07f7bcefaf566
Fernandosoriano <fersoriano@ciencias.unam.mx>
2022-09-23 11:25:59 -0500

    correct_signs,sum_odd_and_even

--- cadenas/2excersisesCadenas.py
+++ cadenas/2excersisesCadenas.py
@@ -4,3 +4,12 @@
 # FUENTE: https://edabit.com/challenge/QFXMcwaQZ8FTAuEtg
 counterpartCharCode = lambda char:ord(char.swapcase())
 # print(counterpartCharCode("a"))
+
+### 2.-Correct Inequality Signs
+# Create a function that returns True if a given inequality expression
+# is correct and False otherwise.
+# FUENTE:https://edabit.com/challenge/eA94BuKYjwMoNQSE2
+correct_signs = lambda txt: eval(txt)
+# print(correct_signs("3 < 7 < 11"))
+# print(correct_signs("13 > 44 > 33 > 1"))
+# print(correct_signs("1 < 2 < 6 < 9 > 3"))
