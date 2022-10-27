d09ff31cba674d6c2adc96c9532c29602fe78ae9
Fernando Valle <fersoriano@ciencias.unam.mx>
2022-10-27 17:26:32 -0500

    add: move_to_end, calculator

--- listas/2listasExercises.py
+++ listas/2listasExercises.py
@@ -6,3 +6,12 @@ find_even_nums = lambda num: [e for e in range (1,num+1) if e %2 == 0]
 # print(find_even_nums(8))
 # print(find_even_nums(4))
 # print(find_even_nums(2))
+
+### 2.-Moving to the End
+# Write a function that moves all elements of one type to the end of the list.
+
+move_to_end = lambda lst, el: [l for l in lst if l !=el]+[l for l in lst if l == el]
+# print(move_to_end([1, 3, 2, 4, 4, 1], 1))
+# print(move_to_end([7, 8, 9, 1, 2, 3, 4], 9))
+# print(move_to_end(["a", "a", "a", "b"], "a"))
+
