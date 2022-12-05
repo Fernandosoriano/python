3d3d517bd0d59b5bc1ad4b7499e49d40389c9fe6
Fernando Valle <fersoriano@ciencias.unam.mx>
2022-12-05 15:08:11 -0600

    add: filter_list, add_indexes, Unpacking Lists

--- listas/2listasExercises.py
+++ listas/2listasExercises.py
@@ -15,3 +15,28 @@ move_to_end = lambda lst, el: [l for l in lst if l !=el]+[l for l in lst if l ==
 # print(move_to_end([7, 8, 9, 1, 2, 3, 4], 9))
 # print(move_to_end(["a", "a", "a", "b"], "a"))
 
+### 3.-Filter out Strings from an Array
+# Create a function that takes a list of non-negative integers and strings and
+# return a new list without the strings.
+filter_list = lambda lst: [i for i in lst if isinstance(i,int)]
+# print(filter_list(["w", "r", "u", 43, "s", "a", 76, "d", 88]))
+
+
+### 4.-Add the Index
+# Given a list of numbers, create a function which returns the list but
+# with each element's index in the list added to itself. This means you add
+# 0 to the number at index 0, add 1 to the number at index 1, etc...
+# FUENTE: https://edabit.com/challenge/gr4ihixfTaoEmZiin
+add_indexes = lambda lst: [value + index for index, value in enumerate(lst)]
+# print(add_indexes([5, 4, 3, 2, 1]))
+
+### 5.-Unpacking Lists
+# Your task is to unpack the list writeyourcodehere into three variables,
+# being first, middle, and last, with middle being everything in between the
+# first and last element. Then print all three variables.
+# Notes
+# Your solution should be just one line of code.
+# If your solution is longer than one line of code, please review the Resources tab.
+# FUENTE: https://edabit.com/challenge/7sdNcax4GsLSrNQbM
+first, *middle, last = [1, 2, 3, 4, 5, 6]
+# print(first, middle, last)
