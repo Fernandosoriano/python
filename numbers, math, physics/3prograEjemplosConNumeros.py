053d69c8ab898252b65dc826e4198bc4d4e2bc67
Fernandosoriano <fersoriano@ciencias.unam.mx>
2024-04-03 14:30:46 -0600

    agrego funcs: square_areas_difference y solve_for_exp

--- numbers, math, physics/3prograEjemplosConNumeros.py
+++ numbers, math, physics/3prograEjemplosConNumeros.py
@@ -63,10 +63,63 @@ def total_overs(balls:int):
 # FUENTE: https://edabit.com/challenge/guR6aa2zytfZvywMS
 # print(total_overs(0)
 
-l:list = ['Glucosa', 'nitrógeno ureico (BUN)', 'urea', 'creatinina', 'ácido úrico', 'calcio total', 
-     'fosforo', 'sodio', 'potasio',
-     'cloro', 'colesterol total', 'triglicéridos',' relación albúmina/globulina', 
-    ' bilirrubina directa', 'bilirrubina indirecta', 'gamma glutamil transpeptidasa (GGT)',
-     'fosfatasa alcalina', 'albúmina', 'bilirrubina total', 'lactato']
-print(len(l))
+
+### 7.-A Circle and Two Squares
+# Imagine a circle and two squares: a smaller and a bigger one. For the smaller one,
+# the circle is a circumcircle and for the bigger one, an incircle.
+
+# Create a function, that takes an integer (radius of the circle) and returns
+# the difference of the areas of the two squares.
+
+# Examples
+# square_areas_difference(5) ➞ 50
+
+# square_areas_difference(6) ➞ 72
+
+# square_areas_difference(7) ➞ 98
+
+
+def square_areas_difference (r:int) -> int|str : 
+    """
+    Doc: https://edabit.com/challenge/NNhkGocuPMcryW7GP
+    
+    Function that takes an integer (radius) and returns
+    the difference between two squres, a smaller and a bigger one. For the
+    smaller one, the circle is a circumcircle and for the bigger one, an incircle.
+    Args:
+        r (int): radius of the circle it must be positive
+    """
+    return  4*r**2 - 2*r**2 if r > 0 else 'Please enter a positive radius'
+
+# print(square_areas_difference(-5))
+
+f = lambda  r: 4*r**2 - 2*r**2 if r > 0 else 'Please enter a positive radius'
+# print(f(5))
+
+### 8.- 
+# import math
+# print (int(math.log(1024, 2)))
+
+
+
+def solve_for_exp(b:int, n:int) -> int:
+    """Function that resolves a logaritm
+       for integers numbers
+
+    Args:
+        b (int): number that acts like the basis of a logaritm
+        n (int): number of  what you want to obtain their logaritm
+
+    Returns:
+        logaritm of one number
+    """
+    e = n / b 
+    l = []
+    while e != 1:
+        e = e / b
+        l.append(e)
+    return len(l)+1
+        
+# print(solve_for_exp(10, 100000000))
+
 
